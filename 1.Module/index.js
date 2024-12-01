038b8ee954452d1effa198bc252b4d868d08110d
web-collab <tanmoy587@gmail.com>
2024-12-01 08:38:24 +0530

    Local Module - exports, require

---
+++ 1.Module/index.js
@@ -0,0 +1,5 @@
+const {getName} = require('./student');
+console.log(getName());
+
+const student = require('./student');
+console.log("student.cgpa: "+student.cgpa);
---
+++ 1.Module/student.js
@@ -0,0 +1,22 @@
+const getName = () =>{
+    return "Bijoy Sen";
+}
+exports.getName=getName;
+
+exports.cgpa = 3.92;
+
+getAge=getAge = () => {
+    return "25";
+}
+
+
+const getMarks= ()=>{
+    return 500;
+}
+
+
+
+const loc = "Kolkata";
+
+const qulification = "MCA"
+
