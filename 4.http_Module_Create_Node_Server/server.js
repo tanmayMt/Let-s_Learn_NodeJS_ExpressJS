dd39e7da44e2ae2fb149f9166329d83996ce3c89
web-collab <tanmoy587@gmail.com>
2024-12-03 10:08:54 +0530

    http Module - Create server with response

--- 4.http_Module_Create_Node_Server/server.js
+++ 4.http_Module_Create_Node_Server/server.js
@@ -7,7 +7,11 @@ const hostname = '127.0.0.1'  //127.0.0.1 <- This address is  localhost
 
 const myServer = http.createServer((req,res)=>{
     // res.end("Hello I am your firt server");
-    res.end("<h1>Hello I am your firt server</h1>");
+    // res.end("<h1>Hello I am your firt server</h1>");
+    res.writeHead(202, {'Content-Type':'text/html'});
+    res.write("<h1>Hello</h1>");
+    res.write("<p>I am your firt server</p>");
+    res.end();
 })
 
 myServer.listen(port, hostname, ()=>{
@@ -15,6 +19,3 @@ myServer.listen(port, hostname, ()=>{
     //console.log(`server is running successfully at http://${hostname}`);
 
 });
-
-
-
