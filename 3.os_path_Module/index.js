80c4d4bdac9307983ff97ff9100b003f93e58ea9
web-collab <tanmoy587@gmail.com>
2024-12-03 09:31:47 +0530

    path Module

--- 3.os_path_Module/index.js
+++ 3.os_path_Module/index.js
@@ -1,8 +1,25 @@
 // os, path
 
-const os = require('os');
-console.log(os.userInfo());
-console.log(os.homedir());
-console.log(os.hostname());
-console.log(os.totalmem());
-console.log(os.freemem());
+// const os = require('os');
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require('path');
+// console.log(path);
+const extaintionName = path.extname("index.html");
+console.log(extaintionName);
+
+const joinName = path.join(__dirname+"/view");
+console.log(joinName);
+
+const joinName1 = path.join(__dirname+"/../view");
+console.log(joinName1);
+
+const joinName2 = path.join(__dirname+"view");
+console.log(joinName2);
