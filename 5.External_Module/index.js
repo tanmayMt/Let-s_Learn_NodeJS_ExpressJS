0868dc9746b7055d96cabc8d72afa0934780d12c
web-collab <tanmoy587@gmail.com>
2024-12-03 21:05:33 +0530

    Extrenal Module - npm(Node Package Manager) movies-names

--- 5.External_Module/index.js
+++ 5.External_Module/index.js
@@ -1,3 +1,5 @@
+console.clear();
+// random-fruits-name <- External Module
 const getRandomFruitsName = require('random-fruits-name')
 
 console.log(getRandomFruitsName()) // Apple - English is default language
@@ -10,3 +12,12 @@ console.log(getRandomFruitsName('fr')) // Pomme
 console.log(getRandomFruitsName('de')) // Apfel
 console.log(getRandomFruitsName('cs')) // Jablko
 console.log(getRandomFruitsName('bn')) // আপেল
+
+
+// movies-names <- External Module
+var moviesNames = require('movies-names');
+console.log(moviesNames.random())
+// moviesNames.all //returns all movies
+// moviesNames.random() //returns a random movie name
+//moviesName.random(2) //returns specified number of random movies
+// moviesNames.getmovieName('The Dark Knight') //returns specified movi
