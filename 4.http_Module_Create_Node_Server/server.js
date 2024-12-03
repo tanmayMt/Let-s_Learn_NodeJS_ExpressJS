264a0de9a5f3c257c17323dac18b9d1e1bd4e624
web-collab <tanmoy587@gmail.com>
2024-12-03 09:55:38 +0530

    http Module - Create server using NodeJS

--- 4.http_Module_Create_Node_Server/server.js
+++ 4.http_Module_Create_Node_Server/server.js
@@ -4,8 +4,13 @@ const port = 3000;
 
 // console.log(http);
 
-http.createServer((req,res)=>{
+
+const myServer = http.createServer((req,res)=>{
     res.end("Hello I am your firt server");
-}).listen(port)
+})
+
+myServer.listen(port,()=>{
+    console.log(`server is running successfully at ${port}`);
+})
 
 
