e16d15f87b733aca09f92ddbe3048bfe0c2fe4e4
web-collab <tanmoy587@gmail.com>
2024-12-03 08:43:42 +0530

    fs  Module -  create and write file

--- 2.fs_Module/index.js
+++ 2.fs_Module/index.js
@@ -1,6 +1,13 @@
 
 const fs = require('fs');
-console.log(fs);
+// console.log(fs);
 
-
+// How to create and write file using asynchronous method
+fs.writeFile("demo1.txt", "My name is Rahul Das", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Successful")
+    }
+});
 
