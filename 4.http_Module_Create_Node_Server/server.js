1d49a84a9e193e5c58b8186d5557af4b847c5020
web-collab <tanmoy587@gmail.com>
2024-12-03 20:56:30 +0530

    Extrenal Module - npm(Node Package Manager) random-fruits-name

--- 4.http_Module_Create_Node_Server/server.js
+++ 4.http_Module_Create_Node_Server/server.js
@@ -6,16 +6,13 @@ const hostname = '127.0.0.1'  //127.0.0.1 <- This address is  localhost
 
 
 const myServer = http.createServer((req,res)=>{
-    // res.end("Hello I am your firt server");
-    // res.end("<h1>Hello I am your firt server</h1>");
-    // res.writeHead(202, {'Content-Type':'text/html'});
+    res.writeHead(202, {'Content-Type':'html'});
     res.write("<h1>Hello</h1>");
-    res.write("<p>I am your firt server</p>");
+    res.write("<p>I am your first server</p>");
     res.end();
 })
 
 myServer.listen(port, hostname, ()=>{
     console.log(`server is running successfully at http://${hostname}:${port}`);
     //console.log(`server is running successfully at http://${hostname}`);
-
 });
---
+++ 5.External_Module/index.js
@@ -0,0 +1,12 @@
+const getRandomFruitsName = require('random-fruits-name')
+
+console.log(getRandomFruitsName()) // Apple - English is default language
+console.log(getRandomFruitsName('en')) // Apple
+console.log(getRandomFruitsName('es')) // Manzana
+console.log(getRandomFruitsName('ja')) // りんご
+console.log(getRandomFruitsName('pt')) // Maça
+console.log(getRandomFruitsName('nl')) // Appel
+console.log(getRandomFruitsName('fr')) // Pomme
+console.log(getRandomFruitsName('de')) // Apfel
+console.log(getRandomFruitsName('cs')) // Jablko
+console.log(getRandomFruitsName('bn')) // আপেল
