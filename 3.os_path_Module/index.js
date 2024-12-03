7a093e076b005e173b7b28aba97ba6deb37d37dd
web-collab <tanmoy587@gmail.com>
2024-12-03 09:17:53 +0530

    os Module

---
+++ 3.os_path_Module/index.js
@@ -0,0 +1,8 @@
+// os, path
+
+const os = require('os');
+console.log(os.userInfo());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.totalmem());
+console.log(os.freemem());
