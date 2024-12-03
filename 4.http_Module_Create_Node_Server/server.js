e120607acca6d39ae2ad52f8143189381026bdcb
web-collab <tanmoy587@gmail.com>
2024-12-03 09:59:17 +0530

    http Module - Create server

--- 4.http_Module_Create_Node_Server/server.js
+++ 4.http_Module_Create_Node_Server/server.js
@@ -1,7 +1,7 @@
 
 const http = require('http');
 const port = 3000;
-
+const hostname = '127.0.0.1'  //127.0.0.1 <- This address is  localhost
 // console.log(http);
 
 
@@ -9,8 +9,11 @@ const myServer = http.createServer((req,res)=>{
     res.end("Hello I am your firt server");
 })
 
-myServer.listen(port,()=>{
-    console.log(`server is running successfully at ${port}`);
-})
+myServer.listen(port, hostname, ()=>{
+    console.log(`server is running successfully at http://${hostname}:${port}`);
+    //console.log(`server is running successfully at http://${hostname}`);
+
+});
+
 
 
