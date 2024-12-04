ae01710dec71153335422e11275b1ac9118c81f4
web-collab <tanmoy587@gmail.com>
2024-12-04 08:51:23 +0530

    Response with html pages for requested url (else if)

--- 6.http_routing_nodemon_package/index.js
+++ 6.http_routing_nodemon_package/index.js
@@ -5,44 +5,43 @@ const hostName = "127.0.0.1";
 
 const server = http.createServer((req,res)=>{
   //console.log(req.url);
-    const handleReadFile = (statusCode, fileLocation) => {
-    fs.readFile(fileLocation, (err, data) => {
-      res.writeHead(statusCode, { "Content-Type": "text/html" });
+  // const handleReadFile = (statusCode, fileLocation) => {
+  //   fs.readFile(fileLocation, (err, data) => {
+  //     res.writeHead(statusCode, { "Content-Type": "text/html" });
+  //     res.write(data);
+  //     res.end();
+  //   });
+  // };
+  if(req.url === "/"){
+    fs.readFile("./views/home.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type": "text/html"});
       res.write(data);
       res.end();
-    });
-  };
-  if(req.url === "/"){
-    // fs.readFile("./views/home.html",(err,data)=>{
-    //   res.writeHead(200,{"Content-Type": "text/html"});
-    //   res.write(data);
-    //   res.end();
-    // })
-    handleReadFile(200, "./views/index.html");
+    })
+    // handleReadFile(200, "./views/index.html");
   }
   else if(req.url === "/about"){
-    // fs.readFile("./views/about.html",(err,data)=>{
-    //   res.writeHead(200,{"Content-Type":"text/html"});
-    //   res.write(data);
-    //   res.end();
-    // })
-    handleReadFile(200, "./views/about.html");
+    fs.readFile("./views/about.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+    //handleReadFile(200, "./views/about.html");
   }
   else if(req.url === "/contact"){
-    // fs.readFile("./views/contact.html",(err,data)=>{
-    //   res.writeHead(200,{"Content-Type":"text/html"});
-    //   res.write(data);
-    //   res.end();
-    // })
-    handleReadFile(200, "./views/contact.html");
+    fs.readFile("./views/contact.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+    //handleReadFile(200, "./views/contact.html");
   }
   else{
-    // fs.readFile("./views/error.html",(err,data)=>{
-    //   res.writeHead(200,{"Content-Type":"text/html"});
-    //   res.write(data);
-    //   res.end();
-    // })
-    handleReadFile(200, "./views/error.html");
+    fs.readFile("./views/error.html",(err,data)=>{
+      res.writeHead(400,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
   }
   // res.end("Welcome to server");
 })
