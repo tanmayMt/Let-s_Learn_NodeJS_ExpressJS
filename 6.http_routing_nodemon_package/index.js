2c7ea4b11a18ac6a9cef17ad95f6f1e04e35d648
web-collab <tanmoy587@gmail.com>
2024-12-04 08:11:35 +0530

    Response with html page for requested url

--- 6.http_routing_nodemon_package/index.js
+++ 6.http_routing_nodemon_package/index.js
@@ -12,6 +12,27 @@ const server = http.createServer((req,res)=>{
       res.end();
     })
   }
+  else if(req.url === "/about"){
+    fs.readFile("about.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+  }
+  else if(req.url === "/contact"){
+    fs.readFile("contact.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+  }
+  else{
+    fs.readFile("error.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+  }
   // res.end("Welcome to server");
 })
 
