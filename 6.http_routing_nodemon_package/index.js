0c8618bba81ff6654df480f7d2268669727bbc56
web-collab <tanmoy587@gmail.com>
2024-12-04 07:58:37 +0530

    Response with html page for requested url

--- 6.http_routing_nodemon_package/index.js
+++ 6.http_routing_nodemon_package/index.js
@@ -1,10 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
 const hostName = "127.0.0.1";
 
 const server = http.createServer((req,res)=>{
   console.log(req.url);
-  res.end("Welcome to server");
+  if(req.url === "/"){
+    fs.readFile("index.html",(error,data)=>{
+      res.writeHead(200,{"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    })
+  }
+  // res.end("Welcome to server");
 })
 
 server.listen(PORT,hostName,()=>{
