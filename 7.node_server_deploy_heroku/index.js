fea1f122d8f9c2fd1731ee8d592821c9c58cad5c
web-collab <tanmoy587@gmail.com>
2024-12-05 07:45:01 +0530

    Create node server

---
+++ 7.node_server_deploy_heroku/index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const hostName = "127.0.0.1";
+
+const server = http.createServer((req,res)=>{
+  //console.log(req.url);
+
+  if(req.url === "/"){
+    fs.readFile("./views/home.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    })
+    //handleReadFile("./views/index.html",200,req,res);
+  }
+  else if(req.url === "/about"){
+    fs.readFile("./views/about.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+    //handleReadFile(200, "./views/about.html");
+  }
+  else if(req.url === "/contact"){
+    fs.readFile("./views/contact.html",(err,data)=>{
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+    //handleReadFile(200, "./views/contact.html");
+  }
+  else{
+    fs.readFile("./views/error.html",(err,data)=>{
+      res.writeHead(400,{"Content-Type":"text/html"});
+      res.write(data);
+      res.end();
+    })
+  }
+  // res.end("Welcome to server");
+})
+
+server.listen(PORT,hostName,()=>{
+  console.log(`Server is running at http://${hostName}:${PORT}`);
+})
