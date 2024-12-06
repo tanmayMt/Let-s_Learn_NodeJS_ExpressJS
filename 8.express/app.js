cc1e6674c10f104b64cce639c0f62eb1b2d94bf8
web-collab <tanmoy587@gmail.com>
2024-12-06 17:47:48 +0530

    Create Routing expressjs Router

--- 8.express/app.js
+++ 8.express/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/users.route")
 
-app.get("/",(req,res)=>{
-    res.send("<h1>I am a get request at Home route</h1>");
-})
+app.use("/api/user/",userRouter);
 
-app.get("/register", (req, res) => {
-  res.send("<h1>I am a get request at register route</h1>")
-});
-app.get("/login", (req, res) => {
-  res.send("<h1>I am a get request at login route</h1>")
+app.use("/",(req,res)=>{
+  res.send("<h1>This is get request at Home route</h1>");
 });
 
 app.use((req, res) => { //For Unknown url which is not mention
