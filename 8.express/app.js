bf9261a1ccd82feb0b2227a15766e461c99b9b92
web-collab <tanmoy587@gmail.com>
2024-12-06 08:23:46 +0530

    express get,post,put,delete request

--- 8.express/app.js
+++ 8.express/app.js
@@ -5,4 +5,16 @@ app.get("/",(req,res)=>{
     res.send("I am a get request at Home route");
 })
 
+app.post("/",(req,res)=>{
+    res.send("I am a post request at Home route");
+})
+
+app.put("/",(req,res)=>{
+    res.send("I am a put request at Home route");
+})
+
+app.delete("/",(req,res)=>{
+    res.send("I am a delete request at Home route");
+})
+
 module.exports = app;
