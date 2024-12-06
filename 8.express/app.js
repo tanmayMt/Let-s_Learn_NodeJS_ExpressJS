5ac758d8e3af143f9c475c0940d952a47a11dad4
web-collab <tanmoy587@gmail.com>
2024-12-06 17:31:24 +0530

    routing

--- 8.express/app.js
+++ 8.express/app.js
@@ -2,19 +2,18 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("I am a get request at Home route");
+    res.send("<h1>I am a get request at Home route</h1>");
 })
 
-app.post("/",(req,res)=>{
-    res.send("I am a post request at Home route");
-})
+app.get("/register", (req, res) => {
+  res.send("<h1>I am a get request at register route</h1>")
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>I am a get request at login route</h1>")
+});
 
-app.put("/",(req,res)=>{
-    res.send("I am a put request at Home route");
-})
-
-app.delete("/",(req,res)=>{
-    res.send("I am a delete request at Home route");
-})
+app.use((req, res) => { //For Unknown url which is not mention
+  res.send("<h1>404 !!! Not a valid url</h1>");
+});
 
 module.exports = app;
