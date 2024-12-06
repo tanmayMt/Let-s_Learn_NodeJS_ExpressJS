d5e8cfb38342b97c2534e24c29f4fc3b5dcee5f7
web-collab <tanmoy587@gmail.com>
2024-12-06 19:10:01 +0530

    Create Routing expressjs Router

--- 8.express/app.js
+++ 8.express/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/users.route")
 
-app.use("/api/user/",userRouter);
+app.use("/api/user",userRouter);
 
-app.use("/",(req,res)=>{
+app.post("/",(req,res)=>{
   res.send("<h1>This is get request at Home route</h1>");
 });
+
 
 app.use((req, res) => { //For Unknown url which is not mention
   res.send("<h1>404 !!! Not a valid url</h1>");
