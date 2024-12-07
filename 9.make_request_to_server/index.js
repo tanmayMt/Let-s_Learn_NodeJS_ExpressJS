a748159c17ee15621de921a893c377225dd838d4
web-collab <tanmoy587@gmail.com>
2024-12-07 10:39:19 +0530

    Make Request with Query Parameter with multiple value distructuring

--- 9.make_request_to_server/index.js
+++ 9.make_request_to_server/index.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = 3001;
 
 app.get("/", (req, res) => {
-  const id = req.query.id;              //http://localhost:3001/?id=101
-  const name = req.query.name;         //http://localhost:3001/?id=101&name=Tanmay 
+  //const id = req.query.id;              //http://localhost:3001/?id=101
+  //const name = req.query.name;         //http://localhost:3001/?id=101&name=Tanmay 
+  const {id,name} =req.query;          //http://localhost:3001/?id=101&name=Tanmay 
+
   res.send(`Student id is : ${id}    &   Student Name is : ${name}`);
 });
 
