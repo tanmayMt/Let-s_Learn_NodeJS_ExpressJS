5d9a485f403cc0be00c7e883c9a0b73d31acd23c
web-collab <tanmoy587@gmail.com>
2024-12-07 19:37:47 +0530

    Make Request using route Parameter(http://localhost:3001/userId/101)

--- 9.make_request_to_server/index.js
+++ 9.make_request_to_server/index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {
-  //const id = req.query.id;              //http://localhost:3001/?id=101
-  //const name = req.query.name;         //http://localhost:3001/?id=101&name=Tanmay 
-  const {id,name} =req.query;          //http://localhost:3001/?id=101&name=Tanmay 
+//Request with Query Parameters
+// app.get("/", (req, res) => {
+//   //const id = req.query.id;              //http://localhost:3001/?id=101
+//   //const name = req.query.name;         //http://localhost:3001/?id=101&name=Tanmay 
+//   const {id,name} =req.query;          //http://localhost:3001/?id=101&name=Tanmay 
 
-  res.send(`Student id is : ${id}    &   Student Name is : ${name}`);
+//   res.send(`Student id is : ${id}    &   Student Name is : ${name}`);
+// });
+
+//Request with Route Parameters
+app.get("/userId/:id/userAge/:age", (req, res) => {  //http://localhost:3001/userId/101/userAge/21
+  const id = req.params.id;
+  const age = req.params.age;
+  res.send(`Student id is : ${id}    &   Student age is : ${age}`);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
