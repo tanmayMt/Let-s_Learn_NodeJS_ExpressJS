10488803d5f550727d05331a1372e4d8a91aaca6
web-collab <tanmoy587@gmail.com>
2024-12-16 08:39:00 +0530

    Common or Single MiddleWare for all/multiple routes

--- 13.Middlewware/index.js
+++ 13.Middlewware/index.js
@@ -7,12 +7,27 @@ const myMiddleWare = (req, res, next) => {
   next();
 };
 
-app.get("/",myMiddleWare,(req, res) => {
+app.use(myMiddleWare); //Common Middleware in app for all routes
+
+app.get("/",(req, res) => {
   // console.log("I am home. ");
-  res.send(`<div>
-              <h1>I am home route</h1>
-              <h4>Date & Time: ${req.curTime}</h4>
-            </div>`);
+  res.send(`<h1>I am home route</h1>
+            <h4>Date & Time: ${req.curTime}</h4>`
+          );
+  res.end();
+});
+
+app.get("/about", (req, res) => {
+  res.send(`<h1>I am about route</h1>
+            <h4>Date & Time: ${req.curTime}</h4>
+          `);
+  res.end();
+});
+
+app.get("/user", (req, res) => {
+  res.send(`<h1>I am user route</h1>
+            <h4>Date & Time: ${req.curTime}</h4>
+          `);
   res.end();
 });
 
