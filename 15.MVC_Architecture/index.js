8dc0667f0909496de9b67dfe4030401098804190
web-collab <tanmoy587@gmail.com>
2024-12-16 19:36:26 +0530

    MCV Achitecture - Create Server & Running it

--- 15.MVC_Architecture/index.js
+++ 15.MVC_Architecture/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-
-
-
+app.get("/",(req,res)=>{
+  res.send("Hello Users");
+})
 app.listen(PORT, () => {
-  console.log(`server is running http://localhost:${PORT}`);
+  console.log(`Server is running http://localhost:${PORT}`);
 });
