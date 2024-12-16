8e9905150c60f7c8b59a9de73816edef3b72539b
web-collab <tanmoy587@gmail.com>
2024-12-16 08:27:06 +0530

    Modification of reqest in MiddleWare Function

--- 13.Middlewware/index.js
+++ 13.Middlewware/index.js
@@ -3,13 +3,17 @@ const app = express();
 const PORT = 3001;
 
 const myMiddleWare = (req, res, next) => {
-  console.log("Middleware function");
+  req.curTime= new Date(Date.now());
   next();
 };
 
 app.get("/",myMiddleWare,(req, res) => {
-  console.log("I am home. ");
-  res.send("<h1>I am home route</h1>");
+  // console.log("I am home. ");
+  res.send(`<div>
+              <h1>I am home route</h1>
+              <h4>Date & Time: ${req.curTime}</h4>
+            </div>`);
+  res.end();
 });
 
 app.listen(PORT, () => {
