cc0dc746838ba5bf49d2b5025b26db132c42ff34
web-collab <tanmoy587@gmail.com>
2024-12-16 19:41:57 +0530

    MCV Achitecture - use middleware for not found page(404)

--- 15.MVC_Architecture/index.js
+++ 15.MVC_Architecture/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/",(req,res)=>{
+app.get("/users",(req,res)=>{
   res.send("Hello Users");
 })
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "resource not found",
+    statusCode: res.statusCode
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
 });
