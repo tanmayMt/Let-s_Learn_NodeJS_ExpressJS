899ba796fee12f0e50bc943f1016212296d53034
web-collab <tanmoy587@gmail.com>
2024-12-16 08:04:39 +0530

    MiddleWare Function (calling next())

--- 13.Middlewware/index.js
+++ 13.Middlewware/index.js
@@ -4,9 +4,10 @@ const PORT = 3001;
 
 const myMiddleWare = (req, res, next) => {
   console.log("Middleware function");
+  next();
 };
 
-app.get("/", myMiddleWare,(req, res) => {
+app.get("/",myMiddleWare,(req, res) => {
   console.log("I am home. ");
   res.send("<h1>I am home route</h1>");
 });
