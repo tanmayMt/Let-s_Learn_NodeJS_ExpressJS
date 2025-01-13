9932885c1a24d3bb9b73b95e688b43ea6fb303aa
web-collab <tanmoy587@gmail.com>
2025-01-13 09:19:53 +0530

    Upload Files to Srever - using multer package

--- 18.Upload_Files_to_Server/controllers/register.controller.js
+++ 18.Upload_Files_to_Server/controllers/register.controller.js
@@ -5,5 +5,7 @@ const getRegister = (req,res)=>{
     res.status(200).sendFile(filePath);
     //res.status(200).sendFile(__dirname+"../views/registerForm.html");
 }
-
-module.exports = getRegister;
+const postRegister = (req,res)=>{
+    res.status(200).send("File is uploaded successfully");
+}
+module.exports = {getRegister,postRegister};
--- 18.Upload_Files_to_Server/routes/register.route.js
+++ 18.Upload_Files_to_Server/routes/register.route.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const getRegister = require("../controllers/register.controller");
+const {getRegister,postRegister} = require("../controllers/register.controller");
 const Router = express.Router();
 
+const multer  = require('multer')
+
+//File Upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const fileName = Date.now() + "-" + file.originalname;
+    cb(null, fileName);
+  }
+})
+const upload = multer({ storage: storage });
+
+
 Router.get("/",getRegister);
+Router.post("/",upload.single("image"),postRegister);
 
 module.exports = Router;
