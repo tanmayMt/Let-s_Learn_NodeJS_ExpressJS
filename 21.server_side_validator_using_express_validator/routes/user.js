37acb5244d064a613e0b65d8448ba7323819f2ca
web-collab <tanmoy587@gmail.com>
2025-01-15 08:51:15 +0530

    Server Side Validator using express validator - name validation with withMessage()

--- 21.server_side_validator_using_express_validator/routes/user.js
+++ 21.server_side_validator_using_express_validator/routes/user.js
@@ -7,7 +7,7 @@ const userRoutes = express.Router();
 
 userRoutes.post(
   "/register",
-  body("name").trim().notEmpty(),
+  body("name").trim().notEmpty().withMessage("Name is missing").isLength({min:5,max:32}).withMessage('name must have at least 5 characters and maximum 32 characters'),
   (req,res,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
