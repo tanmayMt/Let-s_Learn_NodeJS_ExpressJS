3fd7d55e06e38f3574eb1a615533db4616c751b2
web-collab <tanmoy587@gmail.com>
2025-01-15 08:59:38 +0530

    Server Side Validator using express validator - email,password,dob  validation with withMessage()

--- 21.server_side_validator_using_express_validator/routes/user.js
+++ 21.server_side_validator_using_express_validator/routes/user.js
@@ -8,6 +8,9 @@ const userRoutes = express.Router();
 userRoutes.post(
   "/register",
   body("name").trim().notEmpty().withMessage("Name is missing").isLength({min:5,max:32}).withMessage('name must have at least 5 characters and maximum 32 characters'),
+  body("email").trim().notEmpty().withMessage('Email is missing').isEmail().withMessage('Not a valid email'),
+  body('password').trim().notEmpty().withMessage('Password is missing') .isLength({ min: 5 }).withMessage('password must have at least 5 characters'),
+  body('dob') .trim().notEmpty().withMessage('dob is missing').isISO8601().toDate().withMessage('Not a valid dob'),
   (req,res,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
