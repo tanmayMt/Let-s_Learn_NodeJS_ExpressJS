0dd5c71f424b7f89ca53af67f3803d91742a1f6c
web-collab <tanmoy587@gmail.com>
2025-01-16 20:00:52 +0530

    Server Side Validator using express validator - Validation Result Modification

--- 21.server_side_validator_using_express_validator/validation/index.js
+++ 21.server_side_validator_using_express_validator/validation/index.js
@@ -3,7 +3,9 @@ const { check, validationResult } = require('express-validator');
 exports.runValidation = (req, res, next) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()){
-    return res.status(400).json({error:errors.array()});
+    let errorsList = errors.array().map((error) => error.msg);
+    return res.status(422).json({ errors: errorsList });
+    //return res.status(400).json({error:errors.array()});
   }
   next();
 };
