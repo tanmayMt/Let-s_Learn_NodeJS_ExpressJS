d06c965a075dbee0f83ab77fe1c08ff32e2edb2b
tanmayMt <tanmoy587d@gmail.com>
2025-08-07 17:48:24 +0530

    static middleware

---
+++ learn-node-expressjs2/2-expressjs/5-all-static.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//   adding to static assets
+//   SSR
+// })
+
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
